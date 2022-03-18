95dac9c38682f0bf67f261c10f0df97f05263f36
Tantoony <tantoony.id@gmail.com>
2022-03-18 06:07:59 +0300

    .

--- apps/Guard_5/Events/roleUpdate.js
+++ apps/Guard_5/Events/roleUpdate.js
@@ -1,19 +1,18 @@
-const low = require('lowdb');
-const { closeall } = require("../../../HELPERS/functions");
 const Discord = require('discord.js');
-class RoleUpdate {
+const { CliEvent } = require('../../../base/utils');
+class RoleUpdate extends CliEvent {
     constructor(client) {
+        super(client);
         this.client = client;
-    };
+    }
     async run(oldRole, curRole) {
         const client = this.client;
         if (curRole.guild.id !== client.config.server) return;
         const entry = await curRole.guild.fetchAuditLogs({ type: 'ROLE_UPDATE' }).then(logs => logs.entries.first());
-        const utils = await low(client.adapters('utils'));
         if (entry.createdTimestamp <= Date.now() - 5000) return;
         if (entry.executor.id === client.user.id) return;
         const permission = await client.models.perms.findOne({ user: entry.executor.id, type: "update", effect: "role" });
-        if ((permission && (permission.count > 0)) || utils.get("root").value().includes(entry.executor.id)) {
+        if ((permission && (permission.count > 0))) {
             if (permission) await client.models.perms.updateOne({ user: entry.executor.id, type: "update", effect: "role" }, { $inc: { count: -1 } });
             await client.models.bc_roles.updateOne({ _id: curRole.id }, {
                 name: curRole.name,
@@ -30,7 +29,7 @@ class RoleUpdate {
             client.extention.emit('Logger', 'Guard', entry.executor.id, "ROLE_UPDATE", `${oldRole.name} isimli rolü güncelledi. Kalan izin sayısı ${permission ? permission.count - 1 : "sınırsız"}`);
             return;
         }
-        await closeall(curRole.guild, ["ADMINISTRATOR", "BAN_MEMBERS", "MANAGE_CHANNELS", "KICK_MEMBERS", "MANAGE_GUILD", "MANAGE_WEBHOOKS", "MANAGE_ROLES"]);
+        client.extention.emit("Danger", ["ADMINISTRATOR", "BAN_MEMBERS", "MANAGE_CHANNELS", "KICK_MEMBERS", "MANAGE_GUILD", "MANAGE_WEBHOOKS", "MANAGE_ROLES"]);
         const exeMember = curRole.guild.members.cache.get(entry.executor.id);
         client.extention.emit('Jail', exeMember, client.user.id, "KDE - Rol Güncelleme", "Perma", 0);
         await client.models.perms.deleteOne({ user: entry.executor.id, type: "update", effect: "role" });
