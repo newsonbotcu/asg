164dc4947055b76387a02589a57a7b414af1a52c
Tantoony <tantoony.id@gmail.com>
2022-04-26 08:31:52 +0300

    a

--- apps/Moderator/app/Prefix/say.js
+++ apps/Moderator/app/Prefix/say.js
@@ -19,8 +19,8 @@ class Say extends DotCommand {
     async run(client, message, args) {
         await message.reply(new Discord.MessageEmbed().setDescription(stripIndent`
         \`•\` Toplam üye: \`${message.guild.memberCount}\` (${message.guild.members.cache.filter((mem) => mem.presence && mem.presence.status !== 'offline').size} online)
-        \`•\` Booster sayısı: \`${message.guild.members.cache.filter(m => m.roles.cache.has(data.roles["booster"])).size}\` (${message.guild.premiumTier}. seviye)
-        \`•\` Taglı sayısı: \`${message.guild.members.cache.filter(m => client.config.tags[0].some(tag => m.user.username.includes(tag))).size}\` (${message.guild.members.cache.filter(m => m.roles.cache.has(data.roles["cmd-crew"])).size} yetkili)
+        \`•\` Booster sayısı: \`${message.guild.members.cache.filter(m => m.roles.cache.has(client.data.roles["booster"])).size}\` (${message.guild.premiumTier}. seviye)
+        \`•\` Taglı sayısı: \`${message.guild.members.cache.filter(m => client.config.tags[0].some(tag => m.user.username.includes(tag))).size}\` (${message.guild.members.cache.filter(m => m.roles.cache.has(client.data.roles["cmd-crew"])).size} yetkili)
         \`•\` Anlık ses: \`${message.guild.voiceStates.cache.filter(v => v.channel).size}\`
         `).setColor('#7bf3e3'));
     }
