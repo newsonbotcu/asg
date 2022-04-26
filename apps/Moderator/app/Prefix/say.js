2e6a282f41d856da88dab3ea5156117be55c2ee2
Tantoony <tantoony.id@gmail.com>
2022-04-26 08:20:58 +0300

    .

--- apps/Moderator/app/Prefix/say.js
+++ apps/Moderator/app/Prefix/say.js
@@ -1,31 +1,27 @@
 const Discord = require('discord.js');
-const Command = require("../../../Base/Command");
-const low = require('lowdb');
+const { DotCommand } = require("../../../../base/utils");
 const { stripIndent } = require('common-tags');
-class Say extends Command {
+class Say extends DotCommand {
 
     constructor(client) {
         super(client, {
-            name: "say2",
+            name: "say",
             description: "Sunucunun anlık bilgisini verir.",
             usage: "say",
             examples: ["say"],
             category: "Genel",
             accaptedPerms: ["cmd-registry", "cmd-double", "cmd-single", "cmd-ceo"],
             cooldown: 10000,
-            enabled: false
+            enabled: true
         });
     }
 
     async run(client, message, args) {
-        const emojis = await low(client.adapters('emojis'));
-        const channels = await low(client.adapters('channels'));
-        const roles = await low(client.adapters('roles'));
         await message.reply(new Discord.MessageEmbed().setDescription(stripIndent`
         \`•\` Toplam üye: \`${message.guild.memberCount}\` (${message.guild.members.cache.filter(m => m.presence.status !== 'offline').size} online)
         \`•\` Booster sayısı: \`${message.guild.members.cache.filter(m => m.roles.cache.has(data.roles["booster"])).size}\` (${message.guild.premiumTier}. seviye)
         \`•\` Taglı sayısı: \`${message.guild.members.cache.filter(m => client.config.tags[0].some(tag => m.user.username.includes(tag))).size}\` (${message.guild.members.cache.filter(m => m.roles.cache.has(data.roles["cmd-crew"])).size} yetkili)
-        \`•\` Anlık ses: \`${message.guild.voiceStates.cache.filter(v => v.channel).size}\` (${message.guild.voiceStates.cache.filter(v => v.channel && (v.channel.parentId === data.channels["st_public"])).size} public)
+        \`•\` Anlık ses: \`${message.guild.voiceStates.cache.filter(v => v.channel).size}\`
         `).setColor('#7bf3e3'));
     }
 }
