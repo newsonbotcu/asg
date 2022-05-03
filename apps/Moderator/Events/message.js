5cabb43cc08d90f91cbd0f8320fe30fe1b6435f0
Tantoony <tantoony.id@gmail.com>
2022-05-03 16:13:54 +0300

    a

--- apps/Moderator/Events/message.js
+++ apps/Moderator/Events/message.js
@@ -21,7 +21,7 @@ class MsgCrte extends ClientEvent {
                         const reklam = await client.fetchInvite(code);
                         if (!reklam.guild) return;
                         if (reklam.guild.id !== client.guild.id) {
-                            client.emit("ban", message.author.id, client.user.id, `\`${reklam.guild.name}\` [${reklam.guild.id}] sunucusunun reklamını \`${message.channel.name}\` [${message.channel.id}] kanalına attı.`, "p", `#REKLAM`, 2);
+                            client.emit("ban", message.author.id, client.user.id, `\`${reklam.guild.name}\` [${reklam.guild.id}] sunucusunun reklamını\n\`${message.channel.name}\` [${message.channel.id}] kanalına attı.\n${reklam.inviter ? reklam.inviter.username + '#' + reklam.inviter.discriminator + " [" + reklam.inviter.id + "] " : ""}`, "p", `#REKLAM`, 2);
                         }
                     });
                 }
@@ -43,10 +43,10 @@ class MsgCrte extends ClientEvent {
             }
             let count = uCount[message.content] || 0;
             //console.log(uCount);
-            if (count === 1) message.channel.send(`Spamlamaya devam edersen muteleneceksin! ${message.author}`);
+            if (count === 1) message.channel.send(`Spamlamaya devam edersen muteleneceksin! ${ message.author }`);
             if (count === 3) {
                 message.member.roles.add(data.roles["muted"]);
-                message.channel.send(`${message.member} Spam yaptığın için mutelendin!`)
+                message.channel.send(`${ message.member } Spam yaptığın için mutelendin!`)
             }
             if (count >= 1) await message.delete();
             this.client.spamcounts[message.author.id][message.content] = count + 1;
@@ -55,26 +55,26 @@ class MsgCrte extends ClientEvent {
         this.client.spamwait[message.author.id][message.content] = Date.now() + 3000;
         let system = await afkdata.findOne({ _id: message.member.user.id });
         if (system) {
-            message.channel.send(new Discord.MessageEmbed().setDescription(`Seni tekrardan görmek ne güzel ${message.member}!\n${system.inbox.length > 0 ? `${system.inbox.length} yeni mesajın var!\n●▬▬▬▬▬▬▬▬▬●\n${system.inbox.map(content => `[${message.guild.members.cache.get(content.userID) || "Bilinmiyor"}]: ${content.content}`).join('\n')}` : "Hiç yeni mesajın yok!"}`));
+            message.channel.send(new Discord.MessageEmbed().setDescription(`Seni tekrardan görmek ne güzel ${ message.member }!\n${ system.inbox.length > 0 ? `${system.inbox.length} yeni mesajın var!\n●▬▬▬▬▬▬▬▬▬●\n${system.inbox.map(content => `[${message.guild.members.cache.get(content.userID) || "Bilinmiyor"}]: ${content.content}`).join('\n')}` : "Hiç yeni mesajın yok!" }`));
             await afkdata.deleteOne({ _id: message.member.user.id });
         }
         if (message.mentions.members.first()) {
             const afksindata = await afkdata.find();
             const afks = message.mentions.members.array().filter(m => afksindata.some(doc => doc._id === m.user.id));
             if (afks.length > 0) {
-                message.channel.send(new Discord.MessageEmbed().setDescription(afks.map(afk => `${afk} \`${afksindata.find(data => data._id === afk.user.id).reason}\` sebebiyle, **${checkHours(afksindata.find(data => data._id === afk.user.id).created)}** saattir AFK!`).join('\n')));
-                await afks.forEach(async afk => {
-                    await afkdata.updateOne({ _id: afk.user.id }, {
-                        $push: {
-                            inbox: {
-                                content: message.content,
-                                userID: message.author.id
+                message.channel.send(new Discord.MessageEmbed().setDescription(afks.map(afk => `${ afk } \`${afksindata.find(data => data._id === afk.user.id).reason}\` sebebiyle, **${checkHours(afksindata.find(data => data._id === afk.user.id).created)}** saattir AFK!`).join('\n')));
+                    await afks.forEach(async afk => {
+                        await afkdata.updateOne({ _id: afk.user.id }, {
+                            $push: {
+                                inbox: {
+                                    content: message.content,
+                                    userID: message.author.id
+                                }
                             }
-                        }
+                        });
                     });
-                });
+                }
             }
-        }
         */
 
     }
