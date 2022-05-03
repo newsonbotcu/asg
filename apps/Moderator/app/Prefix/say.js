6f1a469c937e5917623d0cab84ea77664ae6f05c
Tantoony <tantoony.id@gmail.com>
2022-05-03 13:36:41 +0300

    a

--- apps/Moderator/app/Prefix/say.js
+++ apps/Moderator/app/Prefix/say.js
@@ -21,7 +21,7 @@ class Say extends DotCommand {
         > <:146_1462:948713818484256808> Toplam üye: \`${message.guild.memberCount}\` (${message.guild.members.cache.filter((mem) => mem.presence && mem.presence.status !== 'offline').size} online)
         > <a:146_raid2:953634074981969961> Booster sayısı: \`${message.guild.members.cache.filter(m => m.roles.cache.has(client.data.roles["booster"])).size}\` (${message.guild.premiumTier.replace("TIER_", "")}. seviye)
         > <:146_asg:967874233810157618> Taglı sayısı: \`${message.guild.members.cache.filter(m => client.config.tags.some(tag => m.user.username.includes(tag)) || client.config.dis === m.user.discriminator).size}\` (${message.guild.members.cache.filter(m => m.roles.cache.has(client.data.roles["cmd-crew"])).size} yetkili)
-        > <:146_1463:948715248477675560> Anlık ses: \`${message.guild.voiceStates.cache.filter(v => v.channel).size}\`
+        > <:146_1463:948715248477675560> Anlık ses: \`${message.guild.members.cache.filter(v => v.voice && v.voice.channel).size}\`
         `);
     }
 }
