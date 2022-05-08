bc15ae50eec755adf938476aafdc901b62628823
Tantoony <tantoony.id@gmail.com>
2022-05-08 23:00:54 +0300

    a

--- apps/Registry/index.js
+++ apps/Registry/index.js
@@ -5,7 +5,6 @@ const client = new Tantoony({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_PRESENCES,
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_INVITES
     ]
 }, __dirname.split('/').pop());
