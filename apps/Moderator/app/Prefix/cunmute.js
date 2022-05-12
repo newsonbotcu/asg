e5c0b05101fdb9e0021b3465aa83539479c5e610
Tantoony <tantoony.id@gmail.com>
2022-05-12 20:39:46 +0300

    a

--- apps/Moderator/app/Prefix/cunmute.js
+++ apps/Moderator/app/Prefix/cunmute.js
@@ -1,11 +1,9 @@
-const Command = require('../../../Base/Command');
-const low = require('lowdb');
-const Mute = require('../../../../../MODELS/Moderation/ChatMuted');
 const Discord = require('discord.js');
 const moment = require("moment");
 moment.locale('tr');
 const { stripIndents } = require('common-tags');
-class cunMute extends Command {
+const { DotCommand } = require("../../../../base/utils");
+class cunMute extends DotCommand {
     constructor(client) {
         super(client, {
             name: "cunmute",
@@ -13,23 +11,18 @@ class cunMute extends Command {
             usage: "cunmute etiket/id",
             examples: ["cunmute 674565119161794560"],
             category: "Moderasyon",
-            aliases: ["cun","unmute"],
-            accaptedPerms: ["root", "owner", "cmd-ceo","cmd-double","cmd-single", "cmd-mute"],
+            aliases: ["cun", "unmute"],
+            accaptedPerms: ["cmute", "yt"],
             cooldown: 10000
         })
     }
     async run(client, message, args) {
-        const roles = await low(client.adapters('roles'));
-        const emojis = await low(client.adapters('emojis'));
-        const channels = await low(client.adapters('channels'));
         let mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mentioned) return message.react(data.emojis["error"].split(':')[2].replace('>', ''));
-        const vData = await Mute.findOne({ _id: mentioned.user.id });
-        if (!vData) return message.react(data.emojis["error"].split(':')[2].replace('>', ''));
-        if (message.guild.members.cache.get(vData.executor).roles.highest.rawPosition > message.member.roles.highest.rawPosition) return message.react(data.emojis["error"].split(':')[2].replace('>', ''));
-        await Mute.deleteOne({ _id: mentioned.user.id });
-        await mentioned.roles.remove(data.roles["muted"]);
-        await message.react(data.emojis["ok"].split(':')[2].replace('>', ''));
+        if (!mentioned) return;// message.react(data.emojis["error"].split(':')[2].replace('>', ''));
+        if (message.guild.members.cache.get(vData.executor).roles.highest.rawPosition > message.member.roles.highest.rawPosition) return;// message.react(client.data.emojis["error"].split(':')[2].replace('>', ''));
+        await client.models.penalties.updateOne({ userId: mentioned.user.id, typeOf: "CMUTE" }, { $set: { until: new Date() } })
+        await mentioned.roles.remove(client.data.roles["muted"]);
+        await message.react("👍");
         /*
         this.client.cmdCooldown[message.author.id][this.info.name] = Date.now() + this.info.cooldown;
         const logChannel = message.guild.channels.cache.get(data.channels["cmd-mod"]);
