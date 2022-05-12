6285a612ac51471944369876ca0cc733e8aba01f
Tantoony <tantoony.id@gmail.com>
2022-05-12 13:58:33 +0300

    a

--- apps/Moderator/Events/voiceStateUpdate.js
+++ apps/Moderator/Events/voiceStateUpdate.js
@@ -13,27 +13,29 @@ class VoiceStateUpdate extends ClientEvent {
             await cur.setMute(true);
         }
         if (prev && cur && prev.selfMute && !cur.selfMute) {
-            let uCooldown = client.trollwait[cur.member.user.id];
+            let uCooldown = client.actionlist.voicespam.get(cur.member.user.id);
             if (!uCooldown) {
-                client.trollwait[cur.member.user.id] = {};
-                uCooldown = client.trollwait[cur.member.user.id];
-            };
-            let time = uCooldown[cur.channel.id] || 0;
-            if (time && (time > Date.now())) {
-                let uCount = client.trollcounts[cur.member.user.id];
-                if (!uCount) {
-                    this.client.trollcounts[cur.member.user.id] = {};
-                    uCount = this.client.trollcounts[cur.member.user.id];
-                };
-                let count = uCount[cur.channel.id] || 0;
-                if (count === 3) await cur.guild.channels.cache.get(this.data.channels["stat-warn"]).send(`${cur.member} Mikrofonun açıp kapamaya devam edersen sesli kanallardan susturulacaksın.`);
-                if (count === 7) {
-                    client.handler.emit("vmute", cur.member, this.client.user.id, "MIC-BUG", 5);
-                    await cur.guild.channels.cache.get(this.data.channels["stat-warn"]).send(`${cur.member} Mikrofonunu çok fazla açıp kapattığın için 5 dakika mutelendin!`);
-                }
-                this.client.trollcounts[cur.member.user.id][cur.channel.id] = count + 1;
+                client.trollwait.set(cur.member.user.id, [
+                    {
+                        channel: cur.channel.id,
+                        date: Date.now()
+                    }
+                ]);
+            } else {
+                const reCdwn = uCooldown.push({
+                    channel: cur.channel.id,
+                    date: Date.now()
+                })
+                client.trollwait.set(cur.member.user.id, reCdwn);
             }
-            this.client.trollwait[cur.member.user.id][cur.channel.id] = Date.now() + 3000;
+            uCooldown = client.actionlist.voicespam.get(cur.member.user.id);
+            let uCount = uCooldown.filter(d => d.channel === cur.channel.id && d.date - Date.now() < 5000);
+            const count = uCount.size;
+            if (count === 3) await cur.guild.channels.cache.get(this.data.channels["chat"]).send(`<@${cur.member.user.id}> Mikrofonun açıp kapamaya devam edersen sesli kanallardan susturulacaksın.`);
+            if (count === 7) {
+                client.handler.emit("vmute", cur.member.user.id, this.client.user.id, "MIC-BUG", 5);
+                await cur.guild.channels.cache.get(this.data.channels["chat"]).send(`<@${cur.member.user.id}> Mikrofonunu çok fazla açıp kapattığın için 5 dakika mutelendin!`);
+            }
         }
         await this.client.models.voice.create({
             channelId: cur.channelId,
